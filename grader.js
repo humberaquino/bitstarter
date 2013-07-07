148afba267ce9077ef43c84d60df26357d80b0df
Humber Aquino <humberaquino@gmail.com>
2013-07-06 20:22:02 -0400

    restlet added to project and grader changed to be able to download resources to check

--- grader.js
+++ grader.js
@@ -24,6 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,23 +39,28 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlString = function(htmlstring) {
+    return cheerio.load(htmlstring);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(file, checksfile) {
+    var fileContent = fs.readFileSync(file);
+    return checkHtmlString(fileContent, checkfile);
+};
+
+var checkHtmlString = function(htmlstring, checksfile) {
+   $ = cheerioHtmlString(htmlstring);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
-    return out;
+    return out; 
 };
 
 var clone = function(fn) {
@@ -61,14 +69,38 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkAndPrintOutput = function(fileContent, checkFile) {
+    var checkJson = checkHtmlString(fileContent, checkFile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'URL file to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    var fileContent = "";
+
+    if (program.url) {
+	//console.log("Download resource: " + program.url);
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+		console.error('Error: ' + result.message);
+	    } else {
+		//console.log("Complete!");
+		checkAndPrintOutput(result, program.checks);
+	    }
+	});
+    } else {
+	// No URL specified. So, Read file
+	// console.log("Processing file: " + program.file);
+	fileContent = fs.readFileSync(program.file);
+	checkAndPrintOutput(fileContent, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlString = checkHtmlString;
 }
